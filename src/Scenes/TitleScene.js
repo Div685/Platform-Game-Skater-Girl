3260387a90fb68af74b58e99f5ca57662908c132
Div685 <divyeshpatel685@gmail.com>
2021-03-31 23:56:39 +0300

    Refactor the code separated Button from scenes

--- src/Scenes/TitleScene.js
+++ src/Scenes/TitleScene.js
@@ -1,5 +1,6 @@
 import 'phaser';
 import config from '../Config/config';
+import Button from '../Objects/Button';
 
 export default class TitleScene extends Phaser.Scene {
   constructor() {
@@ -9,55 +10,63 @@ export default class TitleScene extends Phaser.Scene {
   create () {
 
     // Game
-    this.gameButton = this.add.sprite(100, 200, 'blueButton1').setInteractive();
-    this.centerButton(this.gameButton, 1);
-    
-    this.gameText = this.add.text(0, 0, 'Play', { fontSize: '32px', fill: '#fff' });
-    this.centerButtonText(this.gameText, this.gameButton);
-    
-    this.gameButton.on('pointerdown', function (pointer) {
-      this.scene.start('Game');
-    }.bind(this));
-    
-    this.input.on('pointerover', function (event, gameObjects) {
-      gameObjects[0].setTexture('blueButton2');
-    });
-    
-    this.input.on('pointerout', function (event, gameObjects) {
-      gameObjects[0].setTexture('blueButton1');
-    });
-
+    this.gameButton = new Button(this, config.width/2, config.height/2 - 100, 'blueButton1', 'blueButton2', 'Play', 'Game');
+  
     // Options
-
-    this.optionsButton = this.add.sprite(300,200, 'blueButton1').setInteractive();
-    this.centerButton(this.optionsButton);
-
-    this.optionsText = this.add.text(0, 0, 'Options', { fontSize: '32px', fill: '#fff' });
-    this.centerButtonText(this.optionsText, this.optionsButton);
+    this.optionsButton = new Button(this, config.width/2, config.height/2, 'blueButton1', 'blueButton2', 'Options', 'Options');
+  
+    // Credits
+    this.creditsButton = new Button(this, config.width/2, config.height/2 + 100, 'blueButton1', 'blueButton2', 'Credits', 'Credits');
+ 
+    // this.gameButton = this.add.sprite(100, 200, 'blueButton1').setInteractive();
+    // this.centerButton(this.gameButton, 1);
     
-    this.optionsButton.on('pointerdown', function (pointer) {
-      this.scene.start('Options');
-    }.bind(this));
+    // this.gameText = this.add.text(0, 0, 'Play', { fontSize: '32px', fill: '#fff' });
+    // this.centerButtonText(this.gameText, this.gameButton);
+    
+    // this.gameButton.on('pointerdown', function (pointer) {
+    //   this.scene.start('Game');
+    // }.bind(this));
+    
+    // this.input.on('pointerover', function (event, gameObjects) {
+    //   gameObjects[0].setTexture('blueButton2');
+    // });
+    
+    // this.input.on('pointerout', function (event, gameObjects) {
+    //   gameObjects[0].setTexture('blueButton1');
+    // });
 
-    // Credit
+    // // Options
 
-    this.creditsButton = this.add.sprite(300, 200, 'blueButton1').setInteractive();
-    this.centerButton(this.creditsButton, -1);
+    // this.optionsButton = this.add.sprite(300,200, 'blueButton1').setInteractive();
+    // this.centerButton(this.optionsButton);
 
-    this.creditsText = this.add.text(0, 0, 'Credits', { fontSize: '32px', fill: '#fff' });
-    this.centerButtonText(this.creditsText, this.creditsButton);
+    // this.optionsText = this.add.text(0, 0, 'Options', { fontSize: '32px', fill: '#fff' });
+    // this.centerButtonText(this.optionsText, this.optionsButton);
+    
+    // this.optionsButton.on('pointerdown', function (pointer) {
+    //   this.scene.start('Options');
+    // }.bind(this));
 
-    this.creditsButton.on('pointerdown', function (pointer) {
-      this.scene.start('Credits');
-    }.bind(this));
+    // // Credit
+
+    // this.creditsButton = this.add.sprite(300, 200, 'blueButton1').setInteractive();
+    // this.centerButton(this.creditsButton, -1);
+
+    // this.creditsText = this.add.text(0, 0, 'Credits', { fontSize: '32px', fill: '#fff' });
+    // this.centerButtonText(this.creditsText, this.creditsButton);
+
+    // this.creditsButton.on('pointerdown', function (pointer) {
+    //   this.scene.start('Credits');
+    // }.bind(this));
      
-    this.input.on('pointerover', function (event, gameObjects) {
-      gameObjects[0].setTexture('blueButton2');
-    });
+    // this.input.on('pointerover', function (event, gameObjects) {
+    //   gameObjects[0].setTexture('blueButton2');
+    // });
      
-    this.input.on('pointerout', function (event, gameObjects) {
-      gameObjects[0].setTexture('blueButton1');
-    });
+    // this.input.on('pointerout', function (event, gameObjects) {
+    //   gameObjects[0].setTexture('blueButton1');
+    // });
 
     this.model = this.sys.game.globals.model;
     if (this.model.musicOn === true && this.model.bgMusicPlaying === false) {
@@ -69,18 +78,18 @@ export default class TitleScene extends Phaser.Scene {
 
   }
 
-  centerButton (gameObject, offset = 0) {
-    Phaser.Display.Align.In.Center(
-      gameObject,
-      this.add.zone(config.width/2, config.height/2 - offset * 100, config.width, config.height)
-    );
-  }
+  // centerButton (gameObject, offset = 0) {
+  //   Phaser.Display.Align.In.Center(
+  //     gameObject,
+  //     this.add.zone(config.width/2, config.height/2 - offset * 100, config.width, config.height)
+  //   );
+  // }
    
-  centerButtonText (gameText, gameButton) {
-    Phaser.Display.Align.In.Center(
-      gameText,
-      gameButton
-    );
-  }
+  // centerButtonText (gameText, gameButton) {
+  //   Phaser.Display.Align.In.Center(
+  //     gameText,
+  //     gameButton
+  //   );
+  // }
 
 }
