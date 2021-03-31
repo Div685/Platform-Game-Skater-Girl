e25d9dbea17de30b8018b0dd318c38f9569c53c5
Div685 <divyeshpatel685@gmail.com>
2021-03-31 18:43:25 +0300

    Add OptionsScene content and Model class
    - contains music and sound button

--- src/Scenes/OptionsScene.js
+++ src/Scenes/OptionsScene.js
@@ -10,6 +10,53 @@ export default class OptionsScene extends Phaser.Scene {
   }
 
   create() {
+    this.musicOn = true;
+    this.soundOn = true;
+    
+    this.text = this.add.text(300, 100, 'Options', { fontSize: 40 });
+    this.musicButton = this.add.image(300, 200, 'playButton');
+    this.musicText = this.add.text(350, 190, 'Music Enabled', { fontSize: 24 });
+    
+    this.soundButton = this.add.image(300, 300, 'unmuteButton');
+    this.soundText = this.add.text(350, 290, 'Sound Enabled', { fontSize: 24 });
+    
+    this.musicButton.setInteractive();
+    this.soundButton.setInteractive();
+    
+    this.musicButton.on('pointerdown', function () {
+      this.musicOn = !this.musicOn;
+      this.updateAudio();
+    }.bind(this));
+    
+    this.soundButton.on('pointerdown', function () {
+      this.soundOn = !this.soundOn;
+      this.updateAudio();
+    }.bind(this));
+    
+    this.updateAudio();
+
+    this.menuButton = this.add.sprite(400, 500, 'blueButton1').setInteractive();
+    this.menuText = this.add.text(0, 0, 'Menu', { fontSize: '32px', fill: '#fff' });
+    Phaser.Display.Align.In.Center(this.menuText, this.menuButton);
+    
+    this.menuButton.on('pointerdown', function (pointer) {
+      this.scene.start('Title');
+    }.bind(this));
     
   }
+
+  updateAudio() {
+    if (this.musicOn === false) {
+      this.musicButton.setTexture('stopButton');
+    } else {
+      this.musicButton.setTexture('playButton');
+    }
+   
+    if (this.soundOn === false) {
+      this.soundButton.setTexture('muteButton');
+    } else {
+      this.soundButton.setTexture('unmuteButton');
+    }
+  }
+
 }
--- src/Scenes/PreloaderScene.js
+++ src/Scenes/PreloaderScene.js
@@ -168,7 +168,7 @@ export default class PreloaderScene extends Phaser.Scene {
 
 
   ready () {
-    this.scene.start('Credits');
+    this.scene.start('Options');
     this.readyCount++;
     if (this.readyCount === 2) {
       this.scene.start('Title');
