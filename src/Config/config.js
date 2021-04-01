6069c14a8d039fb19f4b83463ea8bb0f85a2d165
Div685 <divyeshpatel685@gmail.com>
2021-04-01 15:07:42 +0300

    Add player on ground
    - set player on ground position

--- src/Config/config.js
+++ src/Config/config.js
@@ -10,7 +10,7 @@ export default {
   physics: {
     default: 'arcade',
     arcade: {
-      debug: false,
+      debug: true,
     },
   },
 };
--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -11,12 +11,18 @@ export default class GameScene extends Phaser.Scene {
   }
 
   create(){
-    // this.add.image(400, 300, 'logo').setScale(0.2);
+    // ground speed
+    this.gameSpeed = 10;
 
     // Create a Ground
     const {height, width } = this.game.config;
 
-    this.ground = this.add.tileSprite(0, height, 88, 126, 'ground-road').setOrigin(0,1);
+    this.ground = this.add.tileSprite(0, height, width, 126, 'ground-road').setOrigin(0,1);
+    this.skater_girl = this.physics.add.sprite(-80, height, 'skater-girl-roll-0' ).setOrigin(0,1.1).setScale(0.2);
+  }
 
+  // 60 fps
+  update() {
+    this.ground.tilePositionX += this.gameSpeed;
   }
 };
