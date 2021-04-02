60b430ece75d78c549c9daf29e2b94591150ac55
Div685 <divyeshpatel685@gmail.com>
2021-04-02 14:19:48 +0300

    Add startTrigger functionality

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -17,8 +17,13 @@ export default class GameScene extends Phaser.Scene {
     // Create a Ground
     const {height, width } = this.game.config;
 
+    this.isGameRunning = false;
+
+    // trigger for starting game
+    this.startTrigger = this.physics.add.sprite(30, 200).setOrigin(0, 1).setImmovable();
+
     this.ground = this.add.tileSprite(0, height, width, 126, 'ground-road').setOrigin(0,1);
-    this.skater_girl = this.physics.add.sprite(-90, height, 'skater-girl-roll-0')
+    this.skater_girl = this.physics.add.sprite(-105, height, 'skater-girl-roll-0')
     .setOrigin(0,1)
     .setScale(0.2)
     .setBodySize(300, 1700)
@@ -30,7 +35,42 @@ export default class GameScene extends Phaser.Scene {
 
     this.obsticles = this.physics.add.group();
     this.initAnims();
+    this.initStartTrigger();
     this.handleInput();
+  }
+
+  initStartTrigger() {
+    const { width, height } = this.game.config;
+
+    this.physics.add.overlap(this.startTrigger, this.skater_girl, () => {
+      if(this.startTrigger.y === 200) {
+        this.startTrigger.body.reset(30, height);
+        return;
+      }
+
+      this.startTrigger.disableBody(true, true);
+      
+      const startEvent = this.time.addEvent({
+        delay: 1000 / 60,
+        loop: true,
+        callbackScope: this,
+        callback: () => {
+          this.skater_girl.setVelocityX(80);
+          this.skater_girl.play('girl-run', 1);
+
+          if (this.ground.width < width) {
+            this.ground.width += 17 * 2;
+          }
+
+          if (this.ground.width >= width) {
+            this.ground.width = width;
+            this.isGameRunning = true;
+            this.skater_girl.setVelocityX(0);
+            
+          }
+        }
+      });
+    },null, this);
   }
 
   initAnims() {
@@ -82,6 +122,34 @@ export default class GameScene extends Phaser.Scene {
       repeat: -1
     });
 
+    // push start
+    this.anims.create({
+      key: 'girl-push-start',
+      frames: [
+        {key: 'skater-girl-start-0'},
+        {key: 'skater-girl-start-1'},
+        {key: 'skater-girl-start-2'},
+        {key: 'skater-girl-start-3'},
+        {key: 'skater-girl-start-4'},
+        {key: 'skater-girl-start-5'},
+        {key: 'skater-girl-start-6'},
+        {key: 'skater-girl-start-7'},
+        {key: 'skater-girl-start-8'},
+        {key: 'skater-girl-start-9'},
+        {key: 'skater-girl-start-10'},
+        {key: 'skater-girl-start-11'},
+        {key: 'skater-girl-start-12'},
+        {key: 'skater-girl-start-13'},
+        {key: 'skater-girl-start-14'},
+        {key: 'skater-girl-start-15'},
+        {key: 'skater-girl-start-16'},
+        {key: 'skater-girl-start-17'},
+        {key: 'skater-girl-start-18'},
+      ],
+      frameRate: 40,
+      repeat: -1
+    });
+
 
   }
 
@@ -101,6 +169,8 @@ export default class GameScene extends Phaser.Scene {
 
   // 60 fps
   update() {
+    if(!this.isGameRunning){ return; }
+
     this.ground.tilePositionX += this.gameSpeed;
 
     if(this.skater_girl.body.deltaAbsY() > 0) {
@@ -109,7 +179,7 @@ export default class GameScene extends Phaser.Scene {
       // this.skater_girl.body.height <= 58
       //   ? this.skater_girl.play('girl-run', true)
         // : 
-        this.skater_girl.play('girl-run', true);
+        this.skater_girl.play('girl-run');
     }
   }
 };
