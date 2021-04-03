443c868d9912d54203d3be01c4d35f2dc8597749
Div685 <divyeshpatel685@gmail.com>
2021-04-03 23:01:15 +0300

    Add dom element and index.html file
    - for user info

--- src/index.js
+++ src/index.js
@@ -2,7 +2,42 @@ import 'phaser';
 import './style.css';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import checkUser from './utils/checkUser';
+import addPlayer from './utils/addPlayer';
 
+// Dom element
+const buttonActions = document.getElementById('gameStart');
+const formAction = document.getElementById('formDiv');
+const displayUserName = document.getElementById('userName')
+const startGameButton = document.getElementById('btnStart');
+const leaderBoard = document.getElementById('btnLeaderBoard');
+const addUser = document.getElementById('btnSubmit');
+const inputUserName = document.getElementById('uName')
+const form  = document.getElementById('form');
 
+if (checkUser()) {
+  formAction.classList.remove('d-flex');
+  formAction.classList.add('d-none');
+  buttonActions.classList.remove('d-none');
+  buttonActions.classList.add('d-flex');
+  displayUserName.innerHTML = `Hello ${localStorage.getItem('current_player')}!`;
+}
 
-window.game = new Game();
+addUser.addEventListener('click', (e) => {
+  e.preventDefault();
+  const nameValue = inputUserName.value;
+  form.reset();
+  formAction.classList.remove('d-flex');
+  formAction.classList.add('d-none');
+  // loading.classList.remove('d-none');
+  if (nameValue !== '') {
+    const user = addPlayer(nameValue);
+    setTimeout(() => {
+      displayUserName.classList.remove('d-none');
+      displayUserName.classList.add('d-flex');
+      displayUserName.innerHTML = `Hello ${user}!`;
+      buttonActions.classList.remove('d-none');
+    }, 3000);
+  }
+});
+
